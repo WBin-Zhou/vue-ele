4eef6af5a61253c70fbb0e2ab64bae393c2fc467
WBin-Zhou <1925688087@qq.com>
2017-11-04 22:04:47 +0800

    font部分

--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="header">
     <div class="content-wrapper">
-      <div class="avater">
+      <div class="avatar">
         <img :src="seller.avatar" alt="" width="64" height="64">
       </div>
       <div class="contain">
@@ -12,8 +12,8 @@
         <div class="description">
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
-        <div class="support">
-
+        <div class="support" v-if="seller.supports">
+          <span class="icon"></span>
         </div>
       </div>
     </div>
@@ -31,11 +31,43 @@
 </script>
 
 <style lang="scss" type="text/scss">
-#header{
-  color: #fff;
-  background:rgba(7, 17, 27, 0.5);
-  .content-wrapper{
+  @import "../../common/css/mixin";
 
+  #header {
+    color: #fff;
+    background: rgba(7, 17, 27, 0.5);
+    .content-wrapper {
+      padding: 24px 12px 18px 24px;
+      font-size: 0;
+      .avatar {
+        display: inline-block;
+        vertical-align: top;
+        img {
+          border-radius: 2px;
+        }
+      }
+      .contain {
+        display: inline-block;
+        margin-left: 16px;
+        .title {
+          margin: 2px 0 8px 0;
+          .brand {
+            display: inline-block;
+            width: 30px;
+            height: 18px;
+            vertical-align: top;
+            @include bg-image('brand');
+            background-size: 30px 18px;
+            background-repeat: no-repeat;
+          }
+        , name {
+          margin-left: 6px;
+          font-size: 16px;
+          line-height: 18px;
+          font-weight: bold;
+        }
+        }
+      }
+    }
   }
-}
 </style>
